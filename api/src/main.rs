08cdb80889bb2097d52f06c5722029203eb478ac
Kage Edwards <kagemykel.edwards@gmail.com>
2023-07-02 18:21:01 -0700

    Rust backend API, React frontend

---
+++ api/src/main.rs
@@ -0,0 +1,35 @@
+pub mod user;
+
+use tower_http::cors::{Any, CorsLayer};
+use std::net::SocketAddr;
+use axum::{routing::get, response::Json, Router};
+use serde_json::{Value, json};
+
+// use user::Model;
+
+#[tokio::main]
+async fn main() {
+    let cors = CorsLayer::new().allow_origin(Any);
+
+    let app = Router::new()
+        .route("/api", get(root))
+        .layer(cors);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 9000));
+    println!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> Json<Value> {
+    Json(json!({ 
+        "code": 200,
+        "status": "success",
+        "data": {
+            "text" : "It works."
+        }
+    }))
+}
